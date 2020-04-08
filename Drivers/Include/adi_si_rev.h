f2b48532bab85bff59e00637bbda046ceffcc35f
Brian Osama <eacd.brian@gmail.com>
2020-04-09 00:27:19 +0800

    Initial project commit, added aducm405x dfp files

---
+++ Drivers/Include/adi_si_rev.h
@@ -0,0 +1,23 @@
+/*
+ *******************************************************************************
+ * @brief:   Silicon revision information
+ *
+
+*******************************************************************************
+
+ Copyright(c) 2018 Analog Devices, Inc. All Rights Reserved.
+
+ This software is proprietary and confidential.  By using this software you agree
+ to the terms of the associated Analog Devices License Agreement.
+
+ ******************************************************************************/
+
+#ifndef ADI_SI_REV_H
+#define ADI_SI_REV_H
+
+/* List of silicon revisions information used in software components. */
+#define ADUCM4050_SI_REV_0_0        (0u)    /* ADuCM4050 Si. Rev. 0.0 */
+#define ADUCM4050_SI_REV_0_1        (1u)    /* ADuCM4050 Si. Rev. 0.1 */
+
+#endif /* ADI_SI_REV_H */
+
